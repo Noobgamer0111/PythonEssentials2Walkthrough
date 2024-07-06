d09e39b3410c308dbb5f4095b574a3775954b68e
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-06 15:31:34 +1000

    chore: Modify 3.2.4.py

--- Object-Oriented Programming/3.2.4.py
+++ Object-Oriented Programming/3.2.4.py
@@ -1,7 +1,8 @@
-class Stack:  # Defining the Stack class.
-    def __init__(self):  # Defining the constructor function.
-        print("Hi!")
+class Stack:
+    def __init__(self):
+        self.stack_list = []
 
 
-stack_object = Stack()  # Instantiating the object.
+stack_object = Stack()
+print(len(stack_object.stack_list))
     
