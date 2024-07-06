fd034f4e02806211d9563f524b01a53184d5ca4c
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-06 17:39:31 +1000

    Refactor for push function for superclass.

--- Object-Oriented Programming/3.2.5.py
+++ Object-Oriented Programming/3.2.5.py
@@ -1,13 +1,13 @@
 class Stack:
     def __init__(self):
-        self.__stack_list = []
+        self.__stackList = []
 
     def push(self, val):
-        self.__stack_list.append(val)
+        self.__stackList.append(val)
 
     def pop(self):
-        val = self.__stack_list[-1]
-        del self.__stack_list[-1]
+        val = self.__stackList[-1]
+        del self.__stackList[-1]
         return val
 
 
@@ -15,4 +15,7 @@ class AddingStack(Stack):
     def __init__(self):
         Stack.__init__(self)
         self.__sum = 0
+
+
+# Enter code here.
     
