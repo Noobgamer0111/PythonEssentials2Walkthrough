9d73317bd8d6a89708a8bd28755b23e99c133a75
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-06 17:21:23 +1000

    Modified 3.2.5.py for pop and push operations.

--- Object-Oriented Programming/3.2.5.py
+++ Object-Oriented Programming/3.2.5.py
@@ -2,8 +2,10 @@ class Stack:
     def __init__(self):
         self.__stack_list = []
 
+
     def push(self, val):
         self.__stack_list.append(val)
+
 
     def pop(self):
         val = self.__stack_list[-1]
@@ -11,12 +13,12 @@ class Stack:
         return val
 
 
-little_stack = Stack()
-another_stack = Stack()
-funny_stack = Stack()
+stack_object = Stack()
 
-little_stack.push(1)
-another_stack.push(little_stack.pop() + 1)
-funny_stack.push(another_stack.pop() - 2)
+stack_object.push(3)
+stack_object.push(2)
+stack_object.push(1)
 
-print(funny_stack.pop())
+print(stack_object.pop())
+print(stack_object.pop())
+print(stack_object.pop())
