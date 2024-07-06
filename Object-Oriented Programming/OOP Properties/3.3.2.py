9a749f51507f17a6618003203df3ce16fef8efe0
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-06 19:42:07 +1000

    Refactor ExampleClass to use private counter variable and update __dict__ output

--- Object-Oriented Programming/OOP Properties/3.3.2.py
+++ Object-Oriented Programming/OOP Properties/3.3.2.py
@@ -1,14 +1,12 @@
 class ExampleClass:
-    __counter = 0
-    def __init__(self, val = 1):
-        self.__first = val
-        ExampleClass.__counter += 1
+    varia = 2
+    def __init__(self, val):
+        ExampleClass.varia = val
 
 
-example_object_1 = ExampleClass()
-example_object_2 = ExampleClass(2)
-example_object_3 = ExampleClass(4)
+print(ExampleClass.__dict__)
+example_object = ExampleClass(2)
 
-print(example_object_1.__dict__, example_object_1._ExampleClass__counter)
-print(example_object_2.__dict__, example_object_2._ExampleClass__counter)
-print(example_object_3.__dict__, example_object_3._ExampleClass__counter)
+print(ExampleClass.__dict__)
+print(example_object.__dict__)
+    
