b76eb4b4bbd9fac12dbdb35a1920777fa1eb40d8
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-06 19:06:26 +1000

    Refactor 3.3.1.py to include a third attribute and update __dict__ output

--- Object-Oriented Programming/OOP Properties/3.3.1.py
+++ Object-Oriented Programming/OOP Properties/3.3.1.py
@@ -1,9 +1,9 @@
 class ExampleClass:
     def __init__(self, val = 1):
-        self.first = val
+        self.__first = val
 
-    def set_second(self, val):
-        self.second = val
+    def set_second(self, val = 2):
+        self.__second = val
 
 
 example_object_1 = ExampleClass()
@@ -12,9 +12,10 @@ example_object_2 = ExampleClass(2)
 example_object_2.set_second(3)
 
 example_object_3 = ExampleClass(4)
-example_object_3.third = 5
+example_object_3.__third = 5
+
 
 print(example_object_1.__dict__)
 print(example_object_2.__dict__)
 print(example_object_3.__dict__)
-
+    
