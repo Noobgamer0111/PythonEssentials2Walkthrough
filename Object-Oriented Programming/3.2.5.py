fb3a87a359cef070da7e4e2e82b04541258c0bf0
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-06 17:43:23 +1000

    Stack program

--- Object-Oriented Programming/3.2.5.py
+++ Object-Oriented Programming/3.2.5.py
@@ -1,13 +1,13 @@
 class Stack:
     def __init__(self):
-        self.__stackList = []
+        self.__stack_list = []
 
     def push(self, val):
-        self.__stackList.append(val)
+        self.__stack_list.append(val)
 
     def pop(self):
-        val = self.__stackList[-1]
-        del self.__stackList[-1]
+        val = self.__stack_list[-1]
+        del self.__stack_list[-1]
         return val
 
 
@@ -16,6 +16,25 @@ class AddingStack(Stack):
         Stack.__init__(self)
         self.__sum = 0
 
+    def get_sum(self):
+        return self.__sum
 
-# Enter code here.
+    def push(self, val):
+        self.__sum += val
+        Stack.push(self, val)
+
+    def pop(self):
+        val = Stack.pop(self)
+        self.__sum -= val
+        return val
+
+
+stack_object = AddingStack()
+
+for i in range(5):
+    stack_object.push(i)
+print(stack_object.get_sum())
+
+for i in range(5):
+    print(stack_object.pop())
     
