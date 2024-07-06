ed12499631c7a0507bffde49ce2b6d394366c183
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-06 17:30:50 +1000

    chore: Refactor 3.2.5.py for two stacks from same base class

--- Object-Oriented Programming/3.2.5.py
+++ Object-Oriented Programming/3.2.5.py
@@ -2,10 +2,8 @@ class Stack:
     def __init__(self):
         self.__stack_list = []
 
-
     def push(self, val):
         self.__stack_list.append(val)
-
 
     def pop(self):
         val = self.__stack_list[-1]
@@ -13,12 +11,10 @@ class Stack:
         return val
 
 
-stack_object = Stack()
+stack_object_1 = Stack()
+stack_object_2 = Stack()
 
-stack_object.push(3)
-stack_object.push(2)
-stack_object.push(1)
+stack_object_1.push(3)
+stack_object_2.push(stack_object_1.pop())
 
-print(stack_object.pop())
-print(stack_object.pop())
-print(stack_object.pop())
+print(stack_object_2.pop())
