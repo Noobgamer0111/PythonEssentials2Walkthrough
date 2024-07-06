c1afb434bcd1a6f8ca25399b45aa3f4c3d0dbb0c
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-06 15:28:44 +1000

    chore: Add new file

---
+++ Object-Oriented Programming/3.2.4.py
@@ -0,0 +1,7 @@
+class Stack:  # Defining the Stack class.
+    def __init__(self):  # Defining the constructor function.
+        print("Hi!")
+
+
+stack_object = Stack()  # Instantiating the object.
+    
