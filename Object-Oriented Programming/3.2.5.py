d80365c1ebae19041ba5b44a8bd18e141cbed8a3
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-06 17:34:56 +1000

    Invoked superclass constructor.

--- Object-Oriented Programming/3.2.5.py
+++ Object-Oriented Programming/3.2.5.py
@@ -11,12 +11,8 @@ class Stack:
         return val
 
 
-little_stack = Stack()
-another_stack = Stack()
-funny_stack = Stack()
-
-little_stack.push(1)
-another_stack.push(little_stack.pop() + 1)
-funny_stack.push(another_stack.pop() - 2)
-
-print(funny_stack.pop())
+class AddingStack(Stack):
+    def __init__(self):
+        Stack.__init__(self)
+        self.__sum = 0
+    
