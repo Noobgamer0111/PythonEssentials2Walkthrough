75e783cdbfcd9ffd5a85b1ba57e15d2182aa5b25
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-06 18:57:32 +1000

    Refactor Queue and SuperQueue classes for FIFO functionality

--- Object-Oriented Programming/3.2.10 - LAB 3 - Queue aka FIFO part 2.py
+++ Object-Oriented Programming/3.2.10 - LAB 3 - Queue aka FIFO part 2.py
@@ -5,23 +5,22 @@ class QueueError(IndexError):  # Choose base class for the new exception.
 class Queue:
     def __init__(self):
         self.queue = []        
-
     def put(self, elem):
         self.queue.insert(0, elem)
-
     def get(self):
         if len(self.queue) > 0:
-        elem = self.queue[-1]
-        return self.queue.pop()
+            elem = self.queue[-1]
+            return elem
+        else:
+            raise QueueError
     
     def isempty(self):
         return len(self.queue) == 0
 
 
 class SuperQueue(Queue):
-    #
-    # Write new code here.
-    #
+    def isempty(self):
+        return len(self.queue) == 0
 
 
 que = SuperQueue()
