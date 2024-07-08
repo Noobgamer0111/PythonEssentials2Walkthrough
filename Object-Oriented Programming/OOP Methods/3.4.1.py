d983aaf58a62cd8905b07eb124b27481a522c0a1
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-08 15:27:26 +1000

    Refactor Classy method to add varia attribute and print it

--- Object-Oriented Programming/OOP Methods/3.4.1.py
+++ Object-Oriented Programming/OOP Methods/3.4.1.py
@@ -1,9 +1,9 @@
 class Classy:
-    def method(self, par):
-        print("method:", par)
-
-
+    varia = 2
+    def method(self):
+        print(self.varia, self.var)
+ 
+ 
 obj = Classy()
-obj.method(1)
-obj.method(2)
-obj.method(3)
+obj.var = 3
+obj.method()
