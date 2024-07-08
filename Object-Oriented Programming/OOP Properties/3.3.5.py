dbfd19f22c33f58f56b654245da480a89f848e3e
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-08 12:33:43 +1000

    Refactor Python class to distinguish between instance and class variables

---
+++ Object-Oriented Programming/OOP Properties/3.3.5.py
@@ -0,0 +1,21 @@
+# Question 1: Which of the Python class properties are instance variables and which are class variables? Which of them are private?
+
+class Python:
+    population = 1
+    victims = 0
+    def __init__(self):
+        self.length_ft = 3
+        self.__venomous = False
+ 
+# population and victims are class variables, while length and __venomous are instance variables (the latter is also private).
+
+# Question 2: You're going to negate the __venomous property of the version_2 object, ignoring the fact that the property is private. How will you do this?
+
+# input: version_2 = Python()
+ 
+version_3._Python__venomous = not version_3._Python__venomous
+
+# Question 3: Write an expression which checks if the version_2 object contains an instance property named constrictor (yes, constrictor!).
+
+hasattr(version_3, 'constrictor')
+    
