336ebae143c7cd2341a00be06401b1c60e36fd5e
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-08 12:26:07 +1000

    Refactor ExampleClass to use class attribute instead of instance attribute

--- Object-Oriented Programming/OOP Properties/3.3.3.py
+++ Object-Oriented Programming/OOP Properties/3.3.3.py
@@ -1,16 +1,7 @@
 class ExampleClass:
-    def __init__(self, val):
-        if val % 2 != 0:
-            self.a = 1
-        else:
-            self.b = 1
+    attr = 1
 
 
-example_object = ExampleClass(1)
-print(example_object.a)
-
-try:
-    print(example_object.b)
-except AttributeError:
-    pass
+print(hasattr(ExampleClass, 'attr'))
+print(hasattr(ExampleClass, 'prop'))
     
