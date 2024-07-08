3c46362cfe24cc1de7938770b56b98b03d8ddae7
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-08 12:27:28 +1000

    Refactor ExampleClass to use class attribute for 'a' and instance attribute for 'b'

--- Object-Oriented Programming/OOP Properties/3.3.3.py
+++ Object-Oriented Programming/OOP Properties/3.3.3.py
@@ -1,7 +1,13 @@
 class ExampleClass:
-    attr = 1
-
-
-print(hasattr(ExampleClass, 'attr'))
-print(hasattr(ExampleClass, 'prop'))
-    
+    a = 1
+    def __init__(self):
+        self.b = 2
+ 
+ 
+example_object = ExampleClass()
+ 
+print(hasattr(example_object, 'b'))
+print(hasattr(example_object, 'a'))
+print(hasattr(ExampleClass, 'b'))
+print(hasattr(ExampleClass, 'a'))
+ 
