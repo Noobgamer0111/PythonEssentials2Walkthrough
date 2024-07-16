8b6c9ab34daf753afb7834bb292fcbaf3f2a1501
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-16 16:34:34 +1000

    refactor: Update Timer class to include two_digits helper function

---
+++ Object-Oriented Programming/OOP Methods/3.4.7LABTimerClass.py
@@ -0,0 +1,80 @@
+# Initial:
+class Timer:
+    def __init__( ??? ):
+        #
+        # Write code here
+        #
+
+    def __str__(self):
+        #
+        # Write code here
+        #
+
+    def next_second(self):
+        #
+        # Write code here
+        #
+
+    def prev_second(self):
+        #
+        # Write code here
+        #
+
+
+timer = Timer(23, 59, 59)
+print(timer)
+timer.next_second()
+print(timer)
+timer.prev_second()
+print(timer)
+
+# Expected Input:
+
+def two_digits(val):
+    s = str(val)
+    if len(s) == 1:
+        s = '0' + s
+    return s
+
+
+class Timer:
+    def __init__(self, hours=0, minutes=0, seconds=0):
+        self.__hours = hours
+        self.__minutes = minutes
+        self.__seconds = seconds
+
+    def __str__(self):
+        return two_digits(self.__hours) + ":" + \
+               two_digits(self.__minutes) + ":" + \
+               two_digits(self.__seconds)
+
+    def next_second(self):
+        self.__seconds += 1
+        if self.__seconds > 59:
+            self.__seconds = 0
+            self.__minutes += 1
+            if self.__minutes > 59:
+                self.__minutes = 0
+                self.__hours += 1
+                if self.__hours > 23:
+                    self.__hours = 0
+
+    def prev_second(self):
+        self.__seconds -= 1
+        if self.__seconds < 0:
+            self.__seconds = 59
+            self.__minutes -= 1
+            if self.__minutes < 0:
+                self.__minutes = 59
+                self.__hours -= 1
+                if self.__hours < 0:
+                    self.__hours = 23
+
+
+timer = Timer(23, 59, 59)
+print(timer)
+timer.next_second()
+print(timer)
+timer.prev_second()
+print(timer)
+    
