04145f47e9887718e12a6a60bcec6914b2c70b2b
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-17 11:37:09 +1000

    refactor: Update Level1, Level2, and Level3 classes for better inheritance structure
    The code changes update the Level1, Level2, and Level3 classes to improve the inheritance structure. This ensures a more logical and organized hierarchy for the classes.

--- Object-Oriented Programming/OOP Inheritance/3.5.5.py
+++ Object-Oriented Programming/OOP Inheritance/3.5.5.py
@@ -1,17 +1,35 @@
-# Testing properties: instance variables.
-class Super:
+class Level1:
+    variable_1 = 100
     def __init__(self):
-        self.supVar = 11
+        self.var_1 = 101
+
+    def fun_1(self):
+        return 102
 
 
-class Sub(Super):
+class Level2(Level1):
+    variable_2 = 200
     def __init__(self):
         super().__init__()
-        self.subVar = 12
+        self.var_2 = 201
+    
+    def fun_2(self):
+        return 202
 
 
-obj = Sub()
+class Level3(Level2):
+    variable_3 = 300
+    def __init__(self):
+        super().__init__()
+        self.var_3 = 301
 
-print(obj.subVar)
-print(obj.supVar)
+    def fun_3(self):
+        return 302
+
+
+obj = Level3()
+
+print(obj.variable_1, obj.var_1, obj.fun_1())
+print(obj.variable_2, obj.var_2, obj.fun_2())
+print(obj.variable_3, obj.var_3, obj.fun_3())
     
