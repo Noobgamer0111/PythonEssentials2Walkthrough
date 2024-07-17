237c7995ca21811844995293e808732d4aff9717
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-17 12:29:17 +1000

    Added response to Question 2 for 3.5.11 Summary quiz.

--- Object-Oriented Programming/OOP Inheritance/3.5.11SectionQuiz.py
+++ Object-Oriented Programming/OOP Inheritance/3.5.11SectionQuiz.py
@@ -24,8 +24,7 @@ luna = GuardDog("Dobermann")
 
 # Question 1:Question 1: The declaration of the Snake class is given below. 
 # Enrich the class with a method named increment(), adding 1 to the __victims property.
-
-# Solution: Add in "print(rocky) print(luna)" to see the output.
+# Add in "print(rocky) print(luna)" to see the output.
 
 # Answer:
 
@@ -57,4 +56,34 @@ print(luna)
 # Collie says: Woof! Don't run away, Little Lamb!
 # Dobermann says: Woof! Stay where you are, Mister Intruder!
 
-# Question 2: 
+# Question 2: What is the expected output of the following piece of code?
+
+# print(issubclass(SheepDog, Dog), issubclass(SheepDog, GuardDog))
+# print(isinstance(rocky, GuardDog), isinstance(luna, GuardDog))
+
+# Answer: 
+class Dog:
+    kennel = 0
+    def __init__(self, breed):
+        self.breed = breed
+        Dog.kennel += 1
+    def __str__(self):
+        return self.breed + " says: Woof!"
+
+class SheepDog(Dog):
+    def __str__(self):
+        return super().__str__() + " Don't run away, Little Lamb!"
+
+class GuardDog(Dog):
+    def __str__(self):
+        return super().__str__() + " Stay where you are, Mister Intruder!"
+ 
+rocky = SheepDog("Collie")
+luna = GuardDog("Dobermann")
+print(issubclass(SheepDog, Dog), issubclass(SheepDog, GuardDog))
+print(isinstance(rocky, GuardDog), isinstance(luna, GuardDog))
+
+# Expected output:
+# True False
+# False True
+
