53a26748f1e07d3f9e3c970e8b89be5ab1aded25
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-17 12:24:57 +1000

    Added response to Question 1 for 3.5.11 Summary quiz.

--- Object-Oriented Programming/OOP Inheritance/3.5.11SectionQuiz.py
+++ Object-Oriented Programming/OOP Inheritance/3.5.11SectionQuiz.py
@@ -1,4 +1,4 @@
-# Scenario
+# Initial code for the quiz:
 class Dog:
     kennel = 0
     def __init__(self, breed):
@@ -21,8 +21,40 @@ class GuardDog(Dog):
 rocky = SheepDog("Collie")
 luna = GuardDog("Dobermann")
 
-print(rocky)
-print(luna)
+
 # Question 1:Question 1: The declaration of the Snake class is given below. 
 # Enrich the class with a method named increment(), adding 1 to the __victims property.
 
+# Solution: Add in "print(rocky) print(luna)" to see the output.
+
+# Answer:
+
+class Dog:
+    kennel = 0
+    def __init__(self, breed):
+        self.breed = breed
+        Dog.kennel += 1
+    def __str__(self):
+        return self.breed + " says: Woof!"
+ 
+ 
+class SheepDog(Dog):
+    def __str__(self):
+        return super().__str__() + " Don't run away, Little Lamb!"
+ 
+ 
+class GuardDog(Dog):
+    def __str__(self):
+        return super().__str__() + " Stay where you are, Mister Intruder!"
+ 
+ 
+rocky = SheepDog("Collie")
+luna = GuardDog("Dobermann")
+print(rocky)
+print(luna)
+
+# Expected output: 
+# Collie says: Woof! Don't run away, Little Lamb!
+# Dobermann says: Woof! Stay where you are, Mister Intruder!
+
+# Question 2: 
