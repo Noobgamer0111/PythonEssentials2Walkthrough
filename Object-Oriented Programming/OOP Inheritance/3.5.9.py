fd1553283688180744d19164783e85b107a741f2
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-17 11:59:04 +1000

    refactor: Update inheritance hierarchy for Bottom class

--- Object-Oriented Programming/OOP Inheritance/3.5.9.py
+++ Object-Oriented Programming/OOP Inheritance/3.5.9.py
@@ -2,21 +2,17 @@ class Top:
     def m_top(self):
         print("top")
 
-
 class Middle_Left(Top):
     def m_middle(self):
         print("middle_left")
-
 
 class Middle_Right(Top):
     def m_middle(self):
         print("middle_right")
 
-
 class Bottom(Middle_Left, Middle_Right):
 	def m_bottom(self):
 		print("bottom")
-
 
 object = Bottom()
 object.m_bottom()
