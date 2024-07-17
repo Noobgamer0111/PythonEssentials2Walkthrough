1ed9b957ebd9cb9595239f63732f635abb8decd9
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-17 11:32:16 +1000

    refactor: Update Super and Sub classes for better inheritance structure
    The code changes update the Super and Sub classes to improve the inheritance structure. This ensures a more logical and organized hierarchy for the classes.

---
+++ Object-Oriented Programming/OOP Inheritance/3.5.5.py
@@ -0,0 +1,17 @@
+class Super:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return "My name is " + self.name + "."
+
+
+class Sub(Super):
+    def __init__(self, name):
+        Super.__init__(self, name)
+
+
+obj = Sub("Andy")
+
+print(obj)
+    
