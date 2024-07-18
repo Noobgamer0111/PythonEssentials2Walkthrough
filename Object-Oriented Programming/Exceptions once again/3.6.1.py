123da5097ebfe2dd504cb2923a245ba5dd901645
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-18 11:25:25 +1000

    refactor: Improve error handling in reciprocal() function
    The code changes in `3.6.1.py` refactor the `reciprocal()` function to improve error handling. The function now uses a try-except block to catch the `ZeroDivisionError` and returns `None` in such cases. This enhancement makes the code more robust and ensures proper handling of division by zero.

--- Object-Oriented Programming/Exceptions once again/3.6.1.py
+++ Object-Oriented Programming/Exceptions once again/3.6.1.py
@@ -3,17 +3,21 @@ def reciprocal(n):
         n = 1 / n
     except ZeroDivisionError:
         print("Division failed")
-        return None
+        n = None
     else:
         print("Everything went fine")
+    finally:
+        print("It's time to say goodbye")
         return n
 
 
 print(reciprocal(2))
 print(reciprocal(0))
     
-# Expected output: 
+# Output:
 # Everything went fine
+# It's time to say goodbye
 # 0.5
 # Division failed
+# It's time to say goodbye
 # None
