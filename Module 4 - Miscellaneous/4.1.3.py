77b1d170049061b9c5553e8f985b5b0d5423e065
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-18 17:12:04 +1000

    Added powers-of-two generator to 4.1.3.py

--- Module 4 - Miscellaneous/4.1.3.py
+++ Module 4 - Miscellaneous/4.1.3.py
@@ -11,3 +11,23 @@ for v in fun(5):
 # 2
 # 3
 # 4
+
+def powers_of_2(n):
+    power = 1
+    for i in range(n):
+        yield power
+        power *= 2
+
+for v in powers_of_2(8):
+    print(v)
+
+# Expected Output:
+# 1
+# 2
+# 4
+# 8
+# 16
+# 32
+# 64
+# 128
+# 256
