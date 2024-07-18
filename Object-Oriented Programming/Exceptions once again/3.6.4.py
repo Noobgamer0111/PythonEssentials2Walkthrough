a617f4b5d6492891f193fdf443833eb1c81ba069
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-18 12:12:01 +1000

    Added 3.6.4.py

---
+++ Object-Oriented Programming/Exceptions once again/3.6.4.py
@@ -0,0 +1,30 @@
+class MyZeroDivisionError(ZeroDivisionError):	
+    pass
+
+
+def do_the_division(mine):
+    if mine:
+        raise MyZeroDivisionError("some worse news")
+    else:		
+        raise ZeroDivisionError("some bad news")
+
+
+for mode in [False, True]:
+    try:
+        do_the_division(mode)
+    except ZeroDivisionError:
+        print('Division by zero')
+
+for mode in [False, True]:
+    try:
+        do_the_division(mode)
+    except MyZeroDivisionError:
+        print('My division by zero')
+    except ZeroDivisionError:
+        print('Original division by zero')
+    
+# Output:
+# Division by zero
+# Division by zero
+# Original division by zero
+# My division by zero
