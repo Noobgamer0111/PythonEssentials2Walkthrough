4ed9ce4ec8b6823261f7537bcf0459a824ab0908
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-18 12:28:04 +1000

    Add questions and answers for Object-Oriented Programming module quiz 3.7

---
+++ Object-Oriented Programming/3.7ModuleCompletionTest.py
@@ -0,0 +1,61 @@
+# Question 1: A data structure described as LIFO is actually a:
+# tree
+# heap
+# list
+# stack
+
+# A1: stack
+
+# Question 2: If the class’s constructor is declared as below, which one of the assignments is valid?
+class Class:
+
+    def __init__(self):
+
+        pass
+
+# A2: object = Class()
+
+# Question 3: If there is a superclass named A and a subclass named B, which one of the presented invocations should you put instead of the comment?
+# super().__init__()
+
+# Question 4: What will be the effect of running the following code?
+# The code will raise an AttributeError exception
+
+# Question 5: What will be the output of the following code?
+# 2
+
+# Question 6: What will be the output of the following code?
+# 3
+
+# Question 7: What will be the output of the following code?
+# True
+
+# Question 8: What will be the result of executing the following code?
+# False
+
+# Question 9: What will be the result of executing the following code?
+# it will print b
+
+# Question 10: What will be the result of executing the following code?
+# False
+
+# Question 11: What will be the result of executing the following code?
+# it will print b
+
+# Question 12: What will be the result of executing the following code?
+# it will print a
+
+# Question 13: What will be the result of executing the following code?
+# it will print 1
+
+# Question 14: What will be the result of executing the following code?
+# it will print acac
+
+# Question 15: What will be the result of executing the following code?
+# it will print 3
+
+# Question 16: What will be the result of executing the following code?
+# it will print ex
+
+# Question 17: What will be the result of executing the following code?
+# it will print abc
