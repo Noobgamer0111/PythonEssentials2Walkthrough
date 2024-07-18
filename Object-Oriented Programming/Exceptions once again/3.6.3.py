7c5670e4ec52bfbbea56a625c172cf9dc7f3461c
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-18 11:58:23 +1000

    Added 3.6.3.py

---
+++ Object-Oriented Programming/Exceptions once again/3.6.3.py
@@ -0,0 +1,32 @@
+def print_args(args):
+    lng = len(args)
+    if lng == 0:
+        print("")
+    elif lng == 1:
+        print(args[0])
+    else:
+        print(str(args))
+
+
+try:
+    raise Exception
+except Exception as e:
+    print(e, e.__str__(), sep=' : ' ,end=' : ')
+    print_args(e.args)
+
+try:
+    raise Exception("my exception")
+except Exception as e:
+    print(e, e.__str__(), sep=' : ', end=' : ')
+    print_args(e.args)
+
+try:
+    raise Exception("my", "exception")
+except Exception as e:
+    print(e, e.__str__(), sep=' : ', end=' : ')
+    print_args(e.args)
+	
+# Output:
+# :  : 
+# my exception : my exception : my exception
+# ('my', 'exception') : ('my', 'exception') : ('my', 'exception')
