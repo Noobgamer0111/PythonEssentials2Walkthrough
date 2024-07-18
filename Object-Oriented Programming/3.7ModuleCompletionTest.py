3a5c69ef420daca53fe18c6ddd8e8c0d1e8456ff
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-18 12:34:35 +1000

    Reformatted 3.7 Module Completion Test.

--- Object-Oriented Programming/3.7ModuleCompletionTest.py
+++ Object-Oriented Programming/3.7ModuleCompletionTest.py
@@ -8,54 +8,51 @@
 
 # Question 2: If the class’s constructor is declared as below, which one of the assignments is valid?
 class Class:
-
     def __init__(self):
-
         pass
-
 # A2: object = Class()
 
 # Question 3: If there is a superclass named A and a subclass named B, which one of the presented invocations should you put instead of the comment?
-# super().__init__()
+# A3: .__init__(1)
 
 # Question 4: What will be the effect of running the following code?
-# The code will raise an AttributeError exception
+# A4: The code will raise an AttributeError exception
 
 # Question 5: What will be the output of the following code?
-# 2
+# A5: 2
 
 # Question 6: What will be the output of the following code?
-# 3
+# A6: 3
 
 # Question 7: What will be the output of the following code?
-# True
+# A7: True
 
 # Question 8: What will be the result of executing the following code?
-# False
+# A8: False
 
 # Question 9: What will be the result of executing the following code?
-# it will print b
+# A9: it will print b
 
 # Question 10: What will be the result of executing the following code?
-# False
+# A10: False
 
 # Question 11: What will be the result of executing the following code?
-# it will print b
+# A11: it will print b
 
 # Question 12: What will be the result of executing the following code?
-# it will print a
+# A12: it will print a
 
 # Question 13: What will be the result of executing the following code?
-# it will print 1
+# A13: it will print 1
 
 # Question 14: What will be the result of executing the following code?
-# it will print acac
+# A14: it will print acac
 
 # Question 15: What will be the result of executing the following code?
-# it will print 3
+# A15: it will print 3
 
 # Question 16: What will be the result of executing the following code?
-# it will print ex
+# A17: it will print ex
 
 # Question 17: What will be the result of executing the following code?
-# it will print abc
+# A18: it will print abc
