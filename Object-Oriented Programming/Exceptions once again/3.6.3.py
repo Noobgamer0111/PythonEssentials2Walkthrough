fb595d1ac265d7b758201240e963e86eceebbbf3
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-18 12:10:58 +1000

    refactor: corrected code for 3.6.3.py

--- Object-Oriented Programming/Exceptions once again/3.6.3.py
+++ Object-Oriented Programming/Exceptions once again/3.6.3.py
@@ -1,31 +1,32 @@
-class PizzaError(Exception):
-    def __init__(self, pizza, message):
-        Exception.__init__(self, message)
-        self.pizza = pizza
+def print_args(args):
+    lng = len(args)
+    if lng == 0:
+        print("")
+    elif lng == 1:
+        print(args[0])
+    else:
+        print(str(args))
 
 
-class TooMuchCheeseError(PizzaError):
-    def __init__(self, pizza, cheese, message):
-        PizzaError.__init__(self, pizza, message)
-        self.cheese = cheese
+try:
+    raise Exception
+except Exception as e:
+    print(e, e.__str__(), sep=' : ' ,end=' : ')
+    print_args(e.args)
 
+try:
+    raise Exception("my exception")
+except Exception as e:
+    print(e, e.__str__(), sep=' : ', end=' : ')
+    print_args(e.args)
 
-def make_pizza(pizza, cheese):
-    if pizza not in ['margherita', 'capricciosa', 'calzone']:
-        raise PizzaError(pizza, "no such pizza on the menu")
-    if cheese > 100:
-        raise TooMuchCheeseError(pizza, cheese, "too much cheese")
-    print("Pizza ready!")
-
-for (pz, ch) in [('calzone', 0), ('margherita', 110), ('mafia', 20)]:
-    try:
-        make_pizza(pz, ch)
-    except TooMuchCheeseError as tmce:
-        print(tmce, ':', tmce.cheese)
-    except PizzaError as pe:
-        print(pe, ':', pe.pizza)
-        
+try:
+    raise Exception("my", "exception")
+except Exception as e:
+    print(e, e.__str__(), sep=' : ', end=' : ')
+    print_args(e.args)
+	
 # Output:
-# Pizza ready!
-# too much cheese : 110
-# no such pizza on the menu : mafia
+# :  : 
+# my exception : my exception : my exception
+# ('my', 'exception') : ('my', 'exception') : ('my', 'exception')
