25b2bdc2a38d7f4e2e97aef1fca8fa91ecc9e617
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-18 17:02:04 +1000

    Cleaned up code for Fibonacci iterator for cleaner output.

--- Module 4 - Miscellaneous/4.1.1.py
+++ Module 4 - Miscellaneous/4.1.1.py
@@ -1,16 +1,14 @@
 class Fib:
     def __init__(self, nn):
-        print("__init__")
         self.__n = nn
         self.__i = 0
         self.__p1 = self.__p2 = 1
 
     def __iter__(self):
-        print("__iter__")
+        print("Fib iter")
         return self
 
     def __next__(self):
-        print("__next__")		
         self.__i += 1
         if self.__i > self.__n:
             raise StopIteration
@@ -21,31 +19,29 @@ class Fib:
         return ret
 
 
-for i in Fib(10):
-    print(i)
+class Class:
+    def __init__(self, n):
+        self.__iter = Fib(n)
 
-# Output:
-# __init__
-# __iter__
-# __next__
+    def __iter__(self):
+        print("Class iter")
+        return self.__iter
+
+
+object = Class(8)
+
+for i in object:
+    print(i)
+    
+# Expected Output:
+# Class iter
 # 1
-# __next__
 # 1
-# __next__
 # 2
-# __next__
 # 3
-# __next__
 # 5
-# __next__
 # 8
-# __next__
 # 13
-# __next__
 # 21
-# __next__
 # 34
-# __next__
-# 55
-# __next__
-# 89
+# And so on...
