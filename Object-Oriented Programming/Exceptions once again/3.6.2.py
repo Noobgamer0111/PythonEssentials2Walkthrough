10b77f914e7552a70c2bbb147313ffd9f48df13e
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-18 11:37:43 +1000

    Added expected output of 3.6.2.py

--- Object-Oriented Programming/Exceptions once again/3.6.2.py
+++ Object-Oriented Programming/Exceptions once again/3.6.2.py
@@ -1,9 +1,89 @@
-try:
-    i = int("Hello!")
-except Exception as e:
-    print(e)
-    print(e.__str__())
+def print_exception_tree(thisclass, nest = 0):
+    if nest > 1:
+        print("   |" * (nest - 1), end="")
+    if nest > 0:
+        print("   +---", end="")
+
+    print(thisclass.__name__)
+
+    for subclass in thisclass.__subclasses__():
+        print_exception_tree(subclass, nest + 1)
+
+
+print_exception_tree(BaseException)
     
 # Output:
-# invalid literal for int() with base 10: 'Hello!'
-# invalid literal for int() with base 10: 'Hello!'
+# # BaseException
+# #   +---Exception
+# #   |   +---TypeError
+#    |   +---StopAsyncIteration
+#    |   +---StopIteration
+#    |   +---ImportError
+#    |   |   +---ModuleNotFoundError
+#    |   |   +---ZipImportError
+#    |   +---OSError
+#    |   |   +---ConnectionError
+#    |   |   |   +---BrokenPipeError
+#    |   |   |   +---ConnectionAbortedError
+#    |   |   |   +---ConnectionRefusedError
+#    |   |   |   +---ConnectionResetError
+#    |   |   +---BlockingIOError
+#    |   |   +---ChildProcessError
+#    |   |   +---FileExistsError
+#    |   |   +---FileNotFoundError
+#    |   |   +---IsADirectoryError
+#    |   |   +---NotADirectoryError
+#    |   |   +---InterruptedError
+#    |   |   +---PermissionError
+#    |   |   +---ProcessLookupError
+#    |   |   +---TimeoutError
+#    |   |   +---UnsupportedOperation
+#    |   |   +---ItimerError
+#    |   +---EOFError
+#    |   +---RuntimeError
+#    |   |   +---RecursionError
+#    |   |   +---NotImplementedError
+#    |   |   +---_DeadlockError
+#    |   +---NameError
+#    |   |   +---UnboundLocalError
+#    |   +---AttributeError
+#    |   +---SyntaxError
+#    |   |   +---IndentationError
+#    |   |   |   +---TabError
+#    |   +---LookupError
+#    |   |   +---IndexError
+#    |   |   +---KeyError
+#    |   |   +---CodecRegistryError
+#    |   +---ValueError
+#    |   |   +---UnicodeError
+#    |   |   |   +---UnicodeEncodeError
+#    |   |   |   +---UnicodeDecodeError
+#    |   |   |   +---UnicodeTranslateError
+#    |   |   +---UnsupportedOperation
+# ...
+# ...
+#    |   +---AssertionError
+#    |   +---ArithmeticError
+#    |   |   +---FloatingPointError
+#    |   |   +---OverflowError
+#    |   |   +---ZeroDivisionError
+#    |   +---SystemError
+#    |   |   +---CodecRegistryError
+#    |   +---ReferenceError
+#    |   +---MemoryError
+#    |   +---BufferError
+#    |   +---Warning
+#    |   |   +---UserWarning
+#    |   |   +---DeprecationWarning
+#    |   |   +---PendingDeprecationWarning
+#    |   |   +---SyntaxWarning
+#    |   |   +---RuntimeWarning
+#    |   |   +---FutureWarning
+#    |   |   +---ImportWarning
+#    |   |   +---UnicodeWarning
+#    |   |   +---BytesWarning
+#    |   |   +---ResourceWarning
+#    |   +---Error
+#    +---GeneratorExit
+#    +---SystemExit
+#    +---KeyboardInterrupt
