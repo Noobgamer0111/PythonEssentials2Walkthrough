c8b2d122e62f863a50f07498f676ca3fbedc973c
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-19 17:41:07 +1000

    Adjust print_function to remove the poly() function

--- Module 4 - Miscellaneous/4.1.6.py
+++ Module 4 - Miscellaneous/4.1.6.py
@@ -12,3 +12,24 @@ def poly(x):
 
 print_function([x for x in range(-2, 3)], poly)
     
+# Expected output:
+# f(-2)=18
+# f(-1)=8
+# f(0)=2
+# f(1)=0
+# f(2)=2
+
+# Adjusting the print_function to remove the poly() function.
+
+def print_function(args, fun):
+    for x in args:
+        print('f(', x,')=', fun(x), sep='')
+
+print_function([x for x in range(-2, 3)], lambda x: 2 * x**2 - 4 * x + 2)
+
+# Expected output:
+# f(-2)=18
+# f(-1)=8
+# f(0)=2
+# f(1)=0
+# f(2)=2
