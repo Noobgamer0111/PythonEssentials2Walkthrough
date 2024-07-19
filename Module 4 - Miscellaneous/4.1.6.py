b3034c200a0ade9bbbd83891c63e61134422411c
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-19 17:37:50 +1000

    feat: Add function to print the value of a polynomial for a given value of x

--- Module 4 - Miscellaneous/4.1.6.py
+++ Module 4 - Miscellaneous/4.1.6.py
@@ -1,4 +1,6 @@
-# Using the lambda function, write a function that prints the value of a polynomial for a given value of x. The polynomial is f(x) = 2x^2 - 4x + 2. Print the value of the polynomial for x = -2, -1, 0, 1, and 2.
+# Using the lambda function, write a function that prints the value of a polynomial for a
+# given value of x. The polynomial is f(x) = 2x^2 - 4x + 2.
+# Print the value of the polynomial for x = -2, -1, 0, 1, and 2.
 def print_function(args, fun):
     for x in args:
         print('f(', x,')=', fun(x), sep='')
