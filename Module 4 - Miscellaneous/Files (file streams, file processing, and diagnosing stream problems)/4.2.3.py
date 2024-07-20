196328610bcf8c355e8f1f225902f569800ab9f0
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-20 12:17:49 +1000

    chore: Update file handling modes in 4.2.3.py

---
+++ Module 4 - Miscellaneous/Files (file streams, file processing, and diagnosing stream problems)/4.2.3.py
@@ -0,0 +1,7 @@
+# Python is capable of performing the file operations on the file, regardless of the file naming scheme.
+# E.g. Python can perform read and write operations on an opened file.
+
+# Modes to open a stream in Python:
+# 'r' - read mode
+# 'w' - write mode
+# 'a' - append mode/update mode i.e. both read and write to the file.
