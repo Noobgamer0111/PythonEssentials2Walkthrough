79d80c740af332b1c270a198546c235cc2d884b8
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-20 11:49:04 +1000

    chore: Add file stream explanation to 4.2.1.py

---
+++ Module 4 - Miscellaneous/Files (file streams, file processing, and diagnosing stream problems)/4.2.1.py
@@ -0,0 +1,7 @@
+# Python is capable of accessing data in stored files, regardless of media type.
+# This is done through the use of file streams. A file stream is a sequence of data that is accessed 
+# in a sequential manner. The data is read or written to a file, and the file is accessed as a
+# stream of data.
+
+# A typical Python script can be used to process images, perform mathematical calculations, and
+# and calculate statistics.
