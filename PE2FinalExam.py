98e060e13b2d2132fac13809a53c36800673d0c3
Aadil Syed <aadilsyed5117@gmail.com>
2024-07-22 12:35:11 +1000

    chore: Add questions and correct responses for final exam

--- PE2FinalExam.py
+++ PE2FinalExam.py
@@ -1,3 +1,5 @@
+# Questions/Answers for the final exam of the Python Essentials 2 course.
+
 from mod import fun
 # Answer: fun()
 
@@ -8,14 +10,56 @@ print(dir(math))
 
 # file a.py
 print("a", end='')
-
 # file b.py
 import a
 print("b", end='')
-
 # file c.py
 print("c", end='')
 import a
 import b
 
 # Answer: cab
+
+print(__name__)
+# Answer: __main__
+
+from a.b import c
+# Answer:  entity c from module b from package a.
+
+try:
+    raise Exception
+except BaseException:
+    print("a")
+except Exception:
+    print("b")
+except:
+    print("c")
+# Answer: a
+
+for line in open('text.txt', 'rt'):
+# Answer: Invalid. Open is a non-iterable object.
+
+try:
+    raise Exception
+except:
+    print("c")
+except BaseException:
+    print("a")
+except Exception:
+    print("b")
+# Answer: Syntax error.
+
+assert var != 0
+# Answer: Program stops when var == 0.
+
+x = "\\\\"
+print(len(x))
+# Answer: 2
+
+x = "\\\"
+print(len(x))
+# Answer: Will cause an error
+
+print(chr(ord('p') + 2))
+# Answer: r
+
